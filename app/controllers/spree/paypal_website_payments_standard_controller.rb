e483a20127c427d0e8e5fe1c10eb34970b4a036e
Clemens Kofler <clemens@railway.at>
2014-01-27 17:46:45 +0100

    Don't use instance variables

--- app/controllers/spree/paypal_website_payments_standard_controller.rb
+++ app/controllers/spree/paypal_website_payments_standard_controller.rb
@@ -1,19 +1,16 @@
-# TODO get rid of instance variables and before_filters
 module Spree
   # TODO can I use CheckoutController as a base?
   class PaypalWebsitePaymentsStandardController < StoreController
     protect_from_forgery :except => :notify
 
-    before_filter :set_payment, :set_order, :set_payment_method
-
     def pay
       # we need a pending payment if one doesn't exit already
-      payment = @order.payments.with_state('checkout').where(:payment_method_id => @payment_method.id).first_or_create(:amount => @order.total)
+      payment = order.payments.with_state('checkout').where(:payment_method_id => payment_method.id).first_or_create(:amount => order.total)
 
       paypal_cart = Spree::PaypalCart.new(payment,
-        :return        => confirm_paypal_website_payments_standard_url(:payment_id => payment, :token => @order.token),
-        :cancel_return => cancel_paypal_website_payments_standard_url(:payment_id => payment, :token => @order.token),
-        :notify_url    => notify_paypal_website_payments_standard_url(:payment_id => payment, :token => @order.token),
+        :return        => confirm_paypal_website_payments_standard_url(:payment_id => payment, :token => order.token),
+        :cancel_return => cancel_paypal_website_payments_standard_url(:payment_id => payment, :token => order.token),
+        :notify_url    => notify_paypal_website_payments_standard_url(:payment_id => payment, :token => order.token),
       )
 
       redirect_to paypal_cart.redirect_url
@@ -21,10 +18,10 @@ module Spree
 
     def confirm
       # find payment and determine its validity at the same time
-      payment = @order.payments.valid.find_by_id(params[:payment_id])
+      payment = order.payments.valid.find_by_id(params[:payment_id])
 
       if payment.present?
-        PaypalWebsitePaymentsStandardNotification.finalize_order(@order)
+        PaypalWebsitePaymentsStandardNotification.finalize_order(order)
 
         flash[:commerce_tracking] = 'nothing special'
         redirect_to completion_route, :notice => t(:order_processed_successfully)
@@ -39,27 +36,28 @@ module Spree
     end
 
     def cancel
-      redirect_to edit_order_path(@order), :alert => t(:payment_has_been_cancelled)
+      redirect_to edit_order_path(order), :alert => t(:payment_has_been_cancelled)
     end
 
   private
 
     def completion_route
-      order_path(@order)
+      order_path(order)
     end
 
-    def set_payment
-      @payment = Payment.find(params[:payment_id]) if params[:payment_id].present?
+    def payment
+      @payment ||= Payment.find(params[:payment_id]) if params[:payment_id].present?
     end
 
-    def set_order
-      @order = @payment.try(:order) || Order.find_by_number!(params[:order_id])
-      session[:access_token] ||= params[:token]
-      authorize! :edit, @order, session[:access_token]
+    def order
+      @order ||= (payment.try(:order) || Order.find_by_number!(params[:order_id])).tap do |order|
+        session[:access_token] ||= params[:token]
+        authorize! :edit, order, session[:access_token]
+      end
     end
 
-    def set_payment_method
-      @payment_method = @payment.try(:payment_method) || PaymentMethod.find(params[:payment_method_id])
+    def payment_method
+      @payment_method ||= payment.try(:payment_method) || PaymentMethod.find(params[:payment_method_id])
     end
 
   end
