d18f58544e4671dd15606033c3fb6f35b5d65323
Clemens Kofler <clemens@railway.at>
2015-02-18 13:51:08 +0100

    Now there

--- db/migrate/20150218120000_change_unique_index_on_spree_paypal_website_payments_standard_notifications.rb
+++ db/migrate/20150218120000_change_unique_index_on_spree_paypal_website_payments_standard_notifications.rb
@@ -1,7 +1,7 @@
 class ChangeUniqueIndexOnSpreePaypalWebsitePaymentsStandardNotifications < ActiveRecord::Migration
   def up
     add_index :spree_paypal_website_payments_standard_notifications, [:transaction_id, :payment_status], :name => 'unique_index_paypal_standard_notifications'
-    remove_index :spree_paypal_website_payments_standard_notifications, 'unique_idx_paypal_standard_notifications_order_id'
+    remove_index :spree_paypal_website_payments_standard_notifications, :name => 'unique_idx_paypal_standard_notifications_order_id'
   end
 end
 
