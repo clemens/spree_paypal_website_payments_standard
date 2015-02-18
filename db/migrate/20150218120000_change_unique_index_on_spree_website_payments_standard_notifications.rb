2aa4aff84b95553e31e9760434374fee3cbeb934
Clemens Kofler <clemens@railway.at>
2015-02-18 13:31:03 +0100

    Whoopsy

---
+++ db/migrate/20150218120000_change_unique_index_on_spree_website_payments_standard_notifications.rb
@@ -0,0 +1,7 @@
+class ChangeUniqueIndexOnSpreeWebsitePaymentsStandardNotifications < ActiveRecord::Migration
+  def up
+    add_index :spree_website_payments_standard_notifications, [:transaction_id, :payment_status], :name => 'unique_index_paypal_standard_notifications'
+    remove_index :spree_website_payments_standard_notifications, 'unique_idx_paypal_standard_notifications_transaction_id'
+  end
+end
+
